735df3e07140e82cf5e4920034b2d084a1ddae1c
Tiago Lopo Da Silva <tlopo@ibm.com>
2018-01-04 23:51:26 +0100

    Improved tests and rubocop to default rake task

--- lib/tlopo/retry.rb
+++ lib/tlopo/retry.rb
@@ -2,69 +2,68 @@ require 'tlopo/retry/version'
 require 'logger'
 require 'timeout'
 
+# Simple module so we have a namespace
 module Tlopo
   LOGGER ||= proc do
     logger = Logger.new(ENV['TLOPO_LOG_LEVEL'] ? STDERR : '/dev/null')
     logger.level = ENV['TLOPO_LOG_LEVEL'] ? ENV['TLOPO_LOG_LEVEL'].upcase.to_s : Logger::ERROR
     logger
   end.call
-end
 
-module Tlopo::Retry
-  LOGGER = Tlopo::LOGGER
-  LOGGER.debug("#{self} loaded")
-  VALID_OPTS = [ :tries, :timeout, :interval, :fork, :cleanup]
+  # A reusable retry mechanism which supports timeout, cleanup and fork
+  module Retry
+    LOGGER = Tlopo::LOGGER
+    LOGGER.debug("#{self} loaded")
+    VALID_OPTS = %i[tries timeout interval fork cleanup].freeze
 
-  def self.validate_opts(opts)
-    opts.keys.each do |k|
-      msg = "Option #{k} is invalid. Valid options: #{VALID_OPTS}"
-      raise RuntimeError.new msg unless VALID_OPTS.include? k
-    end
-  end 
-
-  def self.retry(opts = {}, &block)
-    validate_opts opts
-    is_fork = opts[:fork]
-    return local(opts, &block) unless is_fork
-    return child(opts, &block) if is_fork
-  end
-
-  def self.child(opts = {}, &block)
-    read, write = IO.pipe
-
-    pid = fork do
-      read.close
-      begin
-        result = local(opts, &block)
-        Marshal.dump({ result: result, error: nil }, write)
-      rescue StandardError => e
-        Marshal.dump({ result: nil, error: e }, write)
+    def self.validate_opts(opts)
+      opts.each_key do |k|
+        msg = "Option #{k} is invalid. Valid options: #{VALID_OPTS}"
+        raise msg unless VALID_OPTS.include?(k)
       end
     end
 
-    write.close
-    result = Marshal.load(read.read)
-    Process.wait(pid)
-    raise result[:error] if result[:error]
-    result[:result]
-  end
+    def self.retry(opts = {}, &block)
+      validate_opts(opts)
+      is_fork = opts[:fork]
+      return local(opts, &block) unless is_fork
+      return child(opts, &block) if is_fork
+    end
 
-  def self.local(opts = {})
-    tries = opts[:tries] || 3
-    timeout = opts[:timeout] || 0
-    interval = opts[:interval] || 1
-    desc = opts[:desc]
-    cleanup = opts[:cleanup]
-    LOGGER.debug("opts: #{opts}")
-    LOGGER.debug("tries: #{tries}")
-    count = 0
-    begin
-      count += 1
-      Timeout.timeout(timeout) { yield }
-    rescue StandardError => e
-      if count > tries
-        raise e
-      else
+    def self.child(opts = {}, &block)
+      read, write = IO.pipe
+
+      pid = fork do
+        read.close
+        begin
+          result = local(opts, &block)
+          Marshal.dump({ result: result, error: nil }, write)
+        rescue StandardError => e
+          Marshal.dump({ result: nil, error: e }, write)
+        end
+      end
+
+      write.close
+      result = Marshal.load(read.read)
+      Process.wait(pid)
+      raise result[:error] if result[:error]
+      result[:result]
+    end
+
+    def self.local(opts = {})
+      tries = get_opt_value(opts, :tries, 3)
+      timeout = get_opt_value(opts, :timeout, 0)
+      interval = get_opt_value(opts, :interval, 1)
+      desc = opts[:desc]
+      cleanup = opts[:cleanup]
+      LOGGER.debug("opts: #{opts}")
+      LOGGER.debug("tries: #{tries}")
+      count = 0
+      begin
+        count += 1
+        Timeout.timeout(timeout) { yield }
+      rescue StandardError => e
+        raise e if count > tries
         msg = "#{self} Retrying to #{desc} #{count} out of #{tries}"
         LOGGER.info(msg) if desc
         LOGGER.debug("#{self} Calling cleanup") if cleanup
@@ -72,6 +71,11 @@ module Tlopo::Retry
         sleep(interval)
         retry
       end
+    end
+
+    def self.get_opt_value(opts, key, default)
+      return opts[key] unless opts[key].nil?
+      default
     end
   end
 end
--- test/tlopo/retry_test.rb
+++ test/tlopo/retry_test.rb
@@ -6,63 +6,66 @@ require 'test_helper'
 # ensure it raises unknown option
 # ensure it calls cleanup
 
-class Tlopo::RetryTest < Minitest::Test
-  def setup
-    @opts = {
-      timeout: 10,
-      desc: 'unit testing'
-    }
-  end
+module Tlopo
+  class RetryTest < Minitest::Test
+    def setup
+      @opts = {
+        timeout: 10,
+        desc: 'unit testing'
+      }
+    end
 
-  def test_that_it_has_a_version_number
-    refute_nil(::Tlopo::Retry::VERSION)
-  end
+    def test_that_it_has_a_version_number
+      refute_nil(::Tlopo::Retry::VERSION)
+    end
 
-  def test_that_it_retries
-    count = 0
-    begin
-      Tlopo::Retry.retry(tries: 1, interval: 0.1 ) do
-        count += 1
-        raise 'whatever'
+    def test_that_it_retries
+      count = 0
+      begin
+        Tlopo::Retry.retry(tries: 1, interval: 0.1) do
+          count += 1
+          raise 'whatever'
+        end
+      rescue RuntimeError => e
+        p(e.message)
       end
-    rescue
+      assert_equal(count, 2)
     end
-    assert_equal count, 2
-  end
 
-  def test_that_it_times_out
-    e = assert_raises Timeout::Error do
-      Tlopo::Retry.retry(timeout: 1) { sleep 2 }
+    def test_that_it_times_out
+      e = assert_raises Timeout::Error do
+        Tlopo::Retry.retry(timeout: 1) { sleep 2 }
+      end
+      assert_equal('execution expired', e.message)
     end
-    assert_equal('execution expired', e.message)
-  end
 
-  def test_that_it_forks
-    parent_pid = Process.pid
-    
-    child_pid = Tlopo::Retry.retry(fork: true) { Process.pid }
-    assert_kind_of Integer, child_pid
-    refute_equal parent_pid, child_pid
-  end
+    def test_that_it_forks
+      parent_pid = Process.pid
 
-  def test_that_it_raises_unknown_opts
-    e = assert_raises RuntimeError do 
-      Tlopo::Retry.retry(unknown: 1) { nil }
+      child_pid = Tlopo::Retry.retry(fork: true) { Process.pid }
+      assert_kind_of(Integer, child_pid)
+      refute_equal(parent_pid, child_pid)
     end
-    assert_match 'unknown', e.message
-  end
 
-  def test_that_it_calls_cleanup
-    desired = 1
-    received = nil
-    begin
-      received = Tlopo::Retry.retry({
-        tries: 1,
-        cleanup: proc { received = desired }
-      }) { raise 'whatever' }
-    rescue
+    def test_that_it_raises_unknown_opts
+      e = assert_raises RuntimeError do
+        Tlopo::Retry.retry(unknown: 1) { nil }
+      end
+      assert_match('unknown', e.message)
     end
-    assert_equal desired, received
-  end
 
+    def test_that_it_calls_cleanup
+      desired = 1
+      received = nil
+      begin
+        received = Tlopo::Retry.retry(
+          tries: 1,
+          cleanup: proc { received = desired }
+        ) { raise 'whatever' }
+      rescue RuntimeError => e
+        p(e.message)
+      end
+      assert_equal(desired, received)
+    end
+  end
 end
