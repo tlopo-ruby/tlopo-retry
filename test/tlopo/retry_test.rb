4768e5fa0409da9397ea57330ab63e8d2e25c036
Tiago Lopo Da Silva <tlopo@ibm.com>
2018-01-04 22:25:16 +0100

    Added more tests

--- test/tlopo/retry_test.rb
+++ test/tlopo/retry_test.rb
@@ -17,6 +17,18 @@ class Tlopo::RetryTest < Minitest::Test
     refute_nil(::Tlopo::Retry::VERSION)
   end
 
+  def test_that_it_retries
+    count = 0
+    begin
+      Tlopo::Retry.retry(tries: 1, interval: 0.1 ) do
+        count += 1
+        raise 'whatever'
+      end
+    rescue
+    end
+    assert_equal count, 2
+  end
+
   def test_that_it_times_out
     e = assert_raises Timeout::Error do
       Tlopo::Retry.retry(timeout: 1) { sleep 2 }
@@ -24,4 +36,13 @@ class Tlopo::RetryTest < Minitest::Test
     File.open('/tmp/1', 'w+') { |f| f.puts([e, e.class, e.message]) }
     assert_equal('execution expired', e.message)
   end
+
+  def test_that_it_forks
+    parent_pid = Process.pid
+    
+    child_pid = Tlopo::Retry.retry(fork: true) { Process.pid }
+    assert_kind_of Integer, child_pid
+    refute_equal parent_pid, child_pid
+  end
+
 end
